cc7cbb8903f631044104d54d90a12774177c042c
Anthony Mai <anthony.mai530@gmail.com>
2020-02-28 04:05:08 +0000

    Automatically backed up by Learn

--- app/views/edit.rb
+++ app/views/edit.rb
@@ -1,5 +1,5 @@
 
- <form action="/articles/#{@article.id}" method="POST">
+ <form action="/articles/#{@article.id}" method="post">
  <input id="hidden" type="hidden" name="_method" value="patch">
  <label for="title">Title:</label>
  <input type="text" id="title" name="title">
